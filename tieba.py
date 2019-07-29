ec475d9def7dcd31c13496c14ca479f2ecf97bf9
gangyu0716 <470487407@qq.com>
2019-07-29 16:05:41 +0800

    feat: add muti page count
    1 add muti page count
    2 fix some syntax error
    
    Signed-off-by: gangyu0716 <470487407@qq.com>

--- tieba.py
+++ tieba.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 import requests
 import re
-import time
 from bs4 import BeautifulSoup
-from collections import Counter
+
 
 def get_html(url):
     try:
@@ -14,16 +13,17 @@ def get_html(url):
     except:
         return "error"
 
+
 def get_content(url):
     comments = []
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
-    #找到每一个主题贴
+    # 找到每一个主题贴
     liTags = soup.find_all('li', attrs={'class': ' j_thread_list clearfix'})
 
-    #print (liTags)
+    # print (liTags)
     for li in liTags:
-        #初始化一个字典用存储
+        # 初始化一个字典用存储
         comment = {}
         try:
             comment['title'] = li.find(
@@ -42,20 +42,36 @@ def get_content(url):
 
     return comments
 
-def keyword_count(dict):
-    weapons = ['单手剑|片手剑','双手剑|双刀|双剑','大剑|重剑',\
-                      '太刀','锤|锤子','狩猎笛|笛子','长枪','铳枪', \
-                      '斩击斧|剑斧','充能斧|盾斧','操虫棍|棍|棍子', \
-                      '弓', '轻弩','重弩']
-    weapons_count = {}
+
+def data_init():
+    global weapons
+    global weapon_count
+
+    # all weapon
+    weapons = ['单手剑|片手剑', '双手剑|双刀|双剑', '大剑|重剑', \
+               '太刀', '锤|锤子', '狩猎笛|笛子', '长枪', '铳枪', \
+               '斩击斧|剑斧', '充能斧|盾斧', '操虫棍|棍|棍子', \
+               '弓', '轻弩', '重弩']
+
+    # init weapon count
+    for weapon in weapons:
+        weapon_count.update({weapon: 0})
+
+    print(weapon_count)
+
+
+def keyword_count(dict,page):
+    global weapons
+    global weapon_count
 
     for weapon in weapons:
         count = 0;
         for comment in dict:
             count = count + len(re.findall(weapon, comment['title']))
-        weapons_count.update({weapon : count})
+        weapon_count.update({weapon : (weapon_count[weapon]+count)})
 
-    print (weapons_count)
+    print("第%d页结果汇总：" % (page+1))
+    print(weapon_count)
 
 
 def out2file(dict):
@@ -63,27 +79,34 @@ def out2file(dict):
         for comment in dict:
             f.write('标题： {} \t 链接：{} \t 发帖人：{} \t 发帖时间：{} \t 回复数量： {} \n'.format(
                 comment['title'], comment['link'], comment['name'], comment['time'], comment['replyNum']))
-    print ('当前页面爬取完成')
+    print('当前页面爬取完成')
+
 
 def main(base_url, deep):
     url_list = []
     for i in range(0,deep):
         url_list.append(base_url + '&pn=' + str(i*50))
 
-    print ('网页信息已经确认')
-    #print (url_list)
+    print('网页信息已经确认')
+    # print (url_list)
 
     for url in url_list:
         content = get_content(url)
-        keyword_count(content)
+        keyword_count(content, url_list.index(url))
         out2file(content)
 
-    print ('所有网页爬取完毕，已输出至文件中')
+    print('所有网页爬取完毕，已输出至文件中')
 
-base_url = 'https://tieba.baidu.com/f?kw=%E6%80%AA%E7%89%A9%E7%8C%8E%E4%BA%BA%E4%B8%96%E7%95%8C&ie=utf-8'
-
-deep = 1
 
 if __name__ == '__main__':
-    main(base_url,deep)
+    weapon_count = {}
+    weapons = []
+    base_url = 'https://tieba.baidu.com/f?kw=%E6%80%AA%E7%89%A9%E7%8C%8E%E4%BA%BA%E4%B8%96%E7%95%8C&ie=utf-8'
+    deep = 3
+
+    data_init()
+    main(base_url, deep)
+
+
+
 
